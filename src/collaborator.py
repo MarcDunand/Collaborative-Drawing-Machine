416c41723edb9339a42e8663f41bda62c81cc0c1
Marc Dunand <marcdunandc@gmail.com>
2025-04-11 19:53:31 -0400

    Introduced arduino control

--- src/collaborator.py
+++ src/collaborator.py
@@ -13,8 +13,13 @@ import threading
 import time
 from PIL import Image, ImageDraw
 import traceback
+import serial
 
 
+
+#Arduino Values
+arduino_port = 'COM8'  #change to match usb port
+baud = 9600
 
 # Define the initial variables
 xDef = 4656  #resolution of the camera
@@ -347,6 +352,12 @@ cv.createTrackbar('Height', 'Positioning', cropH, yDef, on_cropH)
 
 
 
+#Connect to arduino
+ser = serial.Serial(arduino_port, baud)
+time.sleep(2)
+print("Connected to Arduino!")
+
+
 if useVid:
     # Define a video capture object
     vid = cv.VideoCapture(0, cv.CAP_DSHOW)
@@ -423,6 +434,15 @@ while True:
 
     #shows the frame as it will be interpreted by the collaborator
     cv.imshow('Contours and Correction', np.where(processed_frame == 0, 255, 0).astype(np.uint8))
+
+
+
+    #Arduino test
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').strip()
+        print(f"Arduino says: {line}")
+    
+
 
 
     # Key commands
